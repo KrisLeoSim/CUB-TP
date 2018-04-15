45feb338a7fcdb41a6da50b2c1d0c1e6a75c6ad3
SCrolem <darkxield@gmail.com>
2018-04-15 18:37:02 +0100

    gege

--- app/src/main/java/com/example/kris/cubtp/Ficheiro.java
+++ app/src/main/java/com/example/kris/cubtp/Ficheiro.java
@@ -3,20 +3,71 @@ package com.example.kris.cubtp;
 
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.Toast;
+
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Ficheiro {
 
     Context context;
-    String FileName = "CUBFILE.csv";
+    String FileName;
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
 
     public Ficheiro(Context c){
         this.context = c;
+        sharedPref= context.getSharedPreferences("myPref", Context.MODE_PRIVATE);
+        editor=sharedPref.edit();
+
+        criaFicheiro();
     }
 
+    public String gera_nome(){
+        //...
+        return FileName;
+    }
+
+    private void criaFicheiro(){
+        sharedPref.getString("nome_fich",FileName);
+
+        File file = new File(FileName);
+        if(file.exists()){
+            System.out.println("Ficheiro existe ");
+
+
+        }else{
+            System.out.println("Not find file ");
+            // Save your string in SharedPref
+
+            FileName = gera_nome();
+            //gera nome e guarda
+            editor.putString("nome_fich", FileName);
+            editor.commit();
+            saveFile(gera_nome());
+
+        }
+        
+
+
+    }
+
+    public void enviar_fich(){
+
+
+    }
+
+
     public void saveFile(String text){
+
+
+
+
+
         try {
             FileOutputStream fos = context.openFileOutput(FileName,Context.MODE_APPEND);
             fos.write(text.getBytes());
@@ -46,5 +97,10 @@ public class Ficheiro {
         return text;
     }
 
-
+    public String getDataTempo(){
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpledateformat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        String sDate = (String) simpledateformat.format(calendar.getTime());
+        return sDate;
+    }
 }
